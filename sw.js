27279938efaf5ee23ee45a718433475713bce7c8
bonnce <bonce_reloko@hotmail.com>
2022-06-02 11:13:01 -0300

    cache first active

--- sw.js
+++ sw.js
@@ -14,34 +14,20 @@ self.addEventListener("activate", event => {
 });
 
 self.addEventListener("fetch", event => {
-    console.log('event',event.request.url)
-    console.log('caches',caches)
-    caches.open('pwa-assets').then(cache=>{
-        console.log('cache',cache)
-        cache.match(event.request).then(response =>{
-            console.log('response cache',response)
-        })
-    })
-    caches.match(event.request.url).then(response=>{
-        console.log('response caches',response)
-    })
-    fetch(event.request).then(response=>{
-        console.log('fetch response',response)
-    })
-//     event.respondWith(
-//       caches.match(matchRequest).then(cachedResponse => {
-//           const networkFetch = fetch(event.request).then(response => {
-//             // update the cache with a clone of the network response
-//             caches.open("pwa-assets").then(cache => {
-//                 cache.put(event.request, response.clone());
-//             });
-//             console.log(response.clone())
-//           });
-//           // prioritize cached response over network
-//           return cachedResponse || networkFetch;
-//       }
-//     )
-//    )
+    event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+          const networkFetch = fetch(event.request).then(response => {
+            // update the cache with a clone of the network response
+            caches.open("pwa-assets").then(cache => {
+                cache.put(event.request, response.clone());
+            });
+            console.log(response.clone())
+          });
+          // prioritize cached response over network
+          return cachedResponse || networkFetch;
+      }
+    )
+   )
  });
 
  
