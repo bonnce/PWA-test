96c0db144b52a9fabeab61c0c23039e74c250ff0
bonnce <bonce_reloko@hotmail.com>
2022-06-02 00:14:03 -0300

    cache first

--- sw.js
+++ sw.js
@@ -7,6 +7,7 @@ const urlsToCache = ["/PWA-test","style.css"];
 self.addEventListener("install", (event) => {
    event.waitUntil(async () => {
       const cache = await caches.open("pwa-assets");
+      console.log(cache,'cache')
       return cache.addAll(urlsToCache);
    });
    console.log('Service worker installed')
@@ -18,5 +19,9 @@ self.addEventListener("activate", event => {
 
 self.addEventListener("fetch", event => {
     cache.open('pwa-assets').then(cache =>
-    event.respondWith(cache.match(event.request)))
+    event.respondWith(
+        cache.match(event.request)
+        .then(cachedResponse => cachedResponse || fetch(event.request))
+        )
+    )
 });
