392cef8884c174993335cf1a8028b71dd5fc82cd
bonnce <bonce_reloko@hotmail.com>
2022-06-02 02:14:55 -0300

    porfis anda

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@ self.addEventListener("activate", event => {
 });
 
 self.addEventListener("fetch", event => {
-    const matchRequest = event.request.url.replace(/\.io.+/,'.io/')
+    const matchRequest = event.request.url.replace(/\.io.+/,'.io')
     console.log(matchRequest)
     event.respondWith(
       caches.match(matchRequest).then(cachedResponse => {
